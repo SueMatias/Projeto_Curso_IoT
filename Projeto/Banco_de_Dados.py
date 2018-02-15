e8a4035e6754c46c9d0a6f4ee34bd670f0470d50
SueMatias <33202808+SueMatias@users.noreply.github.com>
2018-02-15 15:43:21 -0200

    Add files via upload

--- Projeto/Banco_de_Dados.py
+++ Projeto/Banco_de_Dados.py
@@ -3,9 +3,9 @@ import sqlite3
 import datetime
 import time
 import scipy
-import Principal
+'''import Segundario'''
 
-def criar_tabela():
+def cria_tabela_de_sensores():
 	try:
 		conect = sqlite3.connect('Status.db')
 		cursor = conect.cursor()
@@ -14,9 +14,9 @@ def criar_tabela():
 			CREATE TABLE IF NOT EXISTS Dados (
 				id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 				Tempo TIMESTAMP DEFAULT (DATETIME('now')),
-        			Temperatura REAL, 
-				Umidade REAL, 
-				Luminosidade REAL,
+				Temperatura REAL,
+				Umidade REAL,
+				Luminosidade REAL
 			)
 		""")
 		conect.commit()
@@ -24,17 +24,17 @@ def criar_tabela():
 		print('Banco criado com sucesso.')
 	except Exception as e:
 		print 'except - cria_tabela', e
-	
-def retorno_tabela():
+
+def retorno_dados_sensores():
 	conn = sqlite3.connect('Status.db')
-	cursor = conn.cursor()
+	cursor = conect.cursor()
 	if not quantidade:
 		cursor.execute("""SELECT * FROM Dados ORDER BY datetime(Tempo) ASC""")
 	else:
         	cursor.execute("""SELECT * FROM Dados ORDER BY datetime(Tempo) DESC LIMIT ?""",(quantidade,))
 	return cursor.fetchall()
 
-def insere_dados(Temperatura, Umidade, Luminosidade):
+def adiciona_dados_sensores(Temperatura, Umidade, Luminosidade):
 	Temp = Principal.leitura_temperatura()
 	LDR = Principal.leitura_LDR()
 	Umi = Principal.leitura_umidade()
